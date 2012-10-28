8b1035654accb0461fe540dc397d60f134eb7877
Andy Pliszka <andy@isp44.com>
2012-10-27 23:02:18 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9e4e9632e3c4a3d216e1c092bedf26f054e4363b1d1feb200cbde3505fe93c919ad5f07253f6f44c3c18855cd3405e213463842629518465181b97f8eee8bd72'
